025326dd5b9e2e005876acab846a3a2e5c4743e8
Dylan  Ah Teck <dylanahteck@usc-guestwireless-upc-newsc10548.usc.edu>
2019-06-12 14:44:59 -0700

    added spinner and refactored

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import React, { Fragment, Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/Users';
+import axios from 'axios';
+
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <User />
+          <User loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
